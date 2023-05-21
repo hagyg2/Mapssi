d32a8c257e18d77f266ba9bccad9bb26ce803175
DESKTOP-KC3U4UM\kweou <32190250@dankook.ac.kr>
2023-05-21 16:26:13 +0900

    ui 수정

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -86,80 +86,229 @@ class _WeatherScreenState extends State<WeatherScreen> {
           child: MenuBarDraw(),
         ),
 
-        body: Container(
-            child: Stack(
-              children: [
-              Image.asset('assets/image/background.jpg', fit: BoxFit.cover,
+
+
+      body: Container(
+        child: Stack(
+          children: [
+            Image.asset('assets/image/background.jpg', fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,),
             SingleChildScrollView(
-            child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-            children: [
-            Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                child: Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      SizedBox(height: 100.0,),
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment
-                              .center,
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(
-                              DateFormat(' d MMM (EEEE)').format(
-                                  date), style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.white),
+                            Container(
+                              child: Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(height: 100.0,),
+                                    Container(
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .center,
+                                        children: [
+                                          Text(
+                                            DateFormat(' d MMM (EEEE)').format(
+                                                date), style: TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(height: 5,),
+                                    TimerBuilder.periodic(Duration(minutes: 1),
+                                      builder: (context) {
+                                        print('${getCurrentTime()}');
+                                        return Text('${getCurrentTime()}',
+                                          style: TextStyle(fontSize: 16.0,
+                                              color: Colors.white),);
+                                      },
+                                    ),
+                                    SizedBox(height: 5,),
+                                    Text(('$cityName'), style: TextStyle(
+                                        fontSize: 35.9, color: Colors.white),),
+                                    SizedBox(height: 15.0,),
+                                    Text('$currentTemp°C', style: TextStyle(
+                                        fontSize: 45.0, color: Colors.white),),
+                                    SizedBox(height: 10.0,),
+                                    Text('$tempTMN°C / $tempTMX°C',
+                                      style: TextStyle(
+                                          fontSize: 20.0, color: Colors.white),)
+                                  ],
+                                ),
+                              ),
                             ),
-                          ],
-                        ),
+                            Container(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(height: 80.0,),
+                                  SizedBox(height: 10.0,),
+                                  Text(('습도(%) $cityName'), style: TextStyle(
+                                      fontSize: 10.0, color: Colors.white),),
+                                  SizedBox(height: 5.0,),
+                                  Text(('1시간 강수량(mm) $cityName'),
+                                    style: TextStyle(
+                                        fontSize: 10.0, color: Colors.white),)
+                                ],
+                              ),
+                            ),
+                          ]
                       ),
-                      SizedBox(height: 5,),
-                      TimerBuilder.periodic(Duration(minutes: 1),
-                        builder: (context) {
-                          print('${getCurrentTime()}');
-                          return Text('${getCurrentTime()}',
-                            style: TextStyle(fontSize: 16.0,
-                                color: Colors.white),);
-                        },
+
+
+                      //중앙 부분
+                      SizedBox(height: 20.0),
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(borderRadius: BorderRadius
+                                .circular(20), color: Colors.white),
+                            padding: EdgeInsets.all(17.0),
+                            height: 230.0,
+                            width: 370.0,
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Text('시간대별 날씨', style: TextStyle(
+                                      fontSize: 10.0, color: Colors.black)),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+
+                                    //현재
+                                    Container(
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          SizedBox(height: 10.0,),
+                                          Container(
+                                            child: Column(
+                                                mainAxisAlignment: MainAxisAlignment
+                                                    .spaceEvenly,
+                                                children: [
+                                                  //현재 아이콘
+                                                  SvgPicture.asset(
+                                                    'assets/weather_icon/humidity.svg',),
+                                                  SizedBox(height: 10.0,),
+                                                  Text('현재', style: TextStyle(
+                                                      fontSize: 15.0,
+                                                      color: Colors.black),),
+                                                  SizedBox(
+                                                    height: 10.0,
+                                                  ),
+                                                  Row(
+                                                    children: [
+                                                      SvgPicture.asset(
+                                                        'assets/weather_icon/humidity.svg',
+                                                        width: 30.0,
+                                                        height: 30.0,),
+                                                      Text('$currentTemp°C',
+                                                        style: TextStyle(
+                                                            fontSize: 15.0,
+                                                            color: Colors
+                                                                .black),),
+                                                    ],
+                                                  ),
+                                                  SizedBox(height: 10.0,),
+                                                  Text('$currentTemp°C',
+                                                    style: TextStyle(
+                                                        fontSize: 15.0,
+                                                        color: Colors.black),),
+
+                                                ]
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+
+                                    //1시간 2시간 3시간 뒤(반복)
+                                    Container(
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          SizedBox(height: 10.0,),
+                                          Container(
+                                            child: Column(
+                                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                                children: [
+                                                  //다음날 아이콘
+                                                  SvgPicture.asset(
+                                                    'assets/weather_icon/humidity.svg',),
+                                                  SizedBox(height: 10.0,),
+                                                  Text('$currentTemp°C',
+                                                    style: TextStyle(
+                                                        fontSize: 15.0,
+                                                        color: Colors.white),),
+                                                  SizedBox(height: 10.0,),
+                                                  Row(
+                                                    children: [
+                                                      SvgPicture.asset(
+                                                        'assets/weather_icon/humidity.svg',
+                                                        width: 30.0,
+                                                        height: 30.0,),
+                                                      Text('$currentTemp°C',
+                                                        style: TextStyle(
+                                                            fontSize: 14.0,
+                                                            color: Colors
+                                                                .white),),
+                                                    ],
+                                                  ),
+                                                  SizedBox(height: 10.0,),
+                                                  Text('$currentTemp°C',
+                                                    style: TextStyle(
+                                                        fontSize: 15.0,
+                                                        color: Colors.white),),
+                                                ]
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
                       ),
-                      SizedBox(height: 5,),
-                      Text(('$cityName'), style: TextStyle(
-                          fontSize: 35.9, color: Colors.white),),
-                      SizedBox(height: 15.0,),
-                      Text('$currentTemp°C', style: TextStyle(
-                          fontSize: 45.0, color: Colors.white),),
-                      SizedBox(height: 10.0,),
-                      Text('$tempTMN°C / $tempTMX°C',
-                        style: TextStyle(
-                            fontSize: 20.0, color: Colors.white),)
+                      SizedBox(height: 20,),
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(borderRadius: BorderRadius
+                                .circular(20), color: Colors.white,),
+                            padding: EdgeInsets.all(17.0),
+                            height: 230.0,
+                            width: 370.0,
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Text('주간예보', style: TextStyle(
+                                      fontSize: 15.0, color: Colors.white),),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ],
                   ),
-                ),
-              ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 80.0,),
-                    SizedBox(height: 10.0,),
-                    Text(('습도(%) $cityName'), style: TextStyle(
-                        fontSize: 10.0, color: Colors.white),),
-                    SizedBox(height: 5.0,),
-                    Text(('1시간 강수량(mm) $cityName'),
-                      style: TextStyle(
-                          fontSize: 10.0, color: Colors.white),)
-                  ],
-                ),
-              ),
-            ]
+                )
+            ),
+          ],
         ),
-
-
+      ),
+    );
+  }
+}
 
