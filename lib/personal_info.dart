94838891303ab6a7ca295a96f8dbefab45e32d17
hagyg <hagyg00@naver.com>
2023-10-11 09:31:15 +0900

    선호 스타일 수정

--- lib/personal_info.dart
+++ lib/personal_info.dart
@@ -163,7 +163,11 @@ class percolpage extends StatelessWidget {
                   ),
                   onPressed: () {
                     UserInfo[1] = 'Spring Warm';
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepage()));
+                    if(UserInfo[0] == 1){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepage()));
+                    }
+                    else
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepagew()));
                   },
                 ),
                 SizedBox(height: 10,),
@@ -180,7 +184,11 @@ class percolpage extends StatelessWidget {
                   ),
                   onPressed: () {
                     UserInfo[1] = 'Summer Cool';
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepage()));
+                    if(UserInfo[0] == 1){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepage()));
+                    }
+                    else
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepagew()));
                   },
                 ),
                 SizedBox(height: 10,),
@@ -197,7 +205,11 @@ class percolpage extends StatelessWidget {
                   ),
                   onPressed: () {
                     UserInfo[1] = 'Autumn Warm';
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepage()));
+                    if(UserInfo[0] == 1){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepage()));
+                    }
+                    else
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepagew()));
                   },
                 ),
                 SizedBox(height: 10,),
@@ -214,7 +226,11 @@ class percolpage extends StatelessWidget {
                   ),
                   onPressed: () {
                     UserInfo[1] = 'Winter Cool';
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepage()));
+                    if(UserInfo[0] == 1){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepage()));
+                    }
+                    else
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const prefstylepagew()));
                   },
                 ),
                 SizedBox(height: 10,),
@@ -243,7 +259,7 @@ class percolpage extends StatelessWidget {
   }
 }
 
-//선호 스타일
+//선호 스타일(남)
 class prefstylepage extends StatefulWidget {
   const prefstylepage({Key? key}) : super(key: key);
 
@@ -274,16 +290,10 @@ class _prefstylepage extends State<prefstylepage> {
         UserInfo[2] = 'GoffCore';
       }
       else if(index == 5){
-        UserInfo[2] = 'Lovely';
-      }
-      else if(index == 6){
-        UserInfo[2] = 'Sports';
-      }
-      else if(index == 7){
-        UserInfo[2] = 'Casual';
+        UserInfo[2] = 'Dandy';
       }
       else{
-        UserInfo[2] = 'Casual';
+        UserInfo[2] = 'Sports';
       }
     });
   }
@@ -338,7 +348,7 @@ class _prefstylepage extends State<prefstylepage> {
                           style: ElevatedButton.styleFrom(
                               primary: selectedButtonIndex == 0? Colors.grey : Color(0xffFFFAF3),
                               foregroundColor: Colors.black,
-                              minimumSize: Size(150, 150),
+                              minimumSize: Size(250, 150),
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(7)
                               )
@@ -363,9 +373,12 @@ class _prefstylepage extends State<prefstylepage> {
                           ),
                         ),
                       ),
+                              ]
+                              ),
 
-                      SizedBox(width: 10,height: 10,),
-
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
                       Expanded(
                         child: ElevatedButton(
                           onPressed: () => _handleButtonPress(2),
@@ -379,12 +392,9 @@ class _prefstylepage extends State<prefstylepage> {
                               )
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 10,),
-                  Row(
-                    children: [
+                      ),
+                      SizedBox(width: 10,height: 10,),
+
                       Expanded(
                         child: ElevatedButton(
                           onPressed: () => _handleButtonPress(3),
@@ -416,13 +426,15 @@ class _prefstylepage extends State<prefstylepage> {
                           ),
                         ),
                       ),
-
-                      SizedBox(width: 10,height: 10,),
-
+                  ]
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
                       Expanded(
                         child: ElevatedButton(
                           onPressed: () => _handleButtonPress(5),
-                          child: Text('러블리', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
+                          child: Text('댄디', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
                           style: ElevatedButton.styleFrom(
                               primary: selectedButtonIndex == 5? Colors.grey : Color(0xffFFFAF3),
                               foregroundColor: Colors.black,
@@ -432,12 +444,9 @@ class _prefstylepage extends State<prefstylepage> {
                               )
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 10,),
-                  Row(
-                    children: [
+                      ),
+                      SizedBox(width: 10,height: 10,),
+
                       Expanded(
                         child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
@@ -452,40 +461,272 @@ class _prefstylepage extends State<prefstylepage> {
                           child: Text('스포츠', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
                         ),
                       ),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  ElevatedButton(
+                    child: Text('제출하기', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic'),),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xffFFFAF3),
+                        surfaceTintColor: Color(0xffFFFAF3),
+                        foregroundColor: Colors.black,
+                        minimumSize: Size(280, 40),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)
+                        )
+                    ),
+                    onPressed: ()async{
+                      if(UserInfo[2] != 0){
+                        print(UserInfo[0]);
+                        print(UserInfo[1]);
+                        print(UserInfo[2]);
+                        /*
+                        userController.setUserGender(UserInfo[0] as int?);
+                        userController.setUserPerCol(UserInfo[1].toString());
+                        userController.setUserPrefType(UserInfo[2].toString());
+                        //백엔드로 정보(userinfo) 넘겨주기
+                        if (await sendUserData(userController.getUserId(), UserInfo[0], UserInfo[1], UserInfo[2])) {
+                          //메인 화면으로 이동
+                          Navigator.pushNamedAndRemoveUntil(context, '/index', (route) => false);
+                        }
+                        */
+                        Navigator.pushNamedAndRemoveUntil(context, '/index', (route) => false);
+                      }
+                      else{
+                        showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (BuildContext ctx){
+                              return AlertDialog(
+                                content: Text('선호 스타일을 선택하였는지 확인하여주세요.', style: TextStyle(fontSize: 15, fontFamily: 'Dovemayo_gothic'),),
+                                actions: [
+                                  Center(
+                                    child: TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('확인', style: TextStyle(fontSize: 15, fontFamily: 'Dovemayo_gothic'),),
+                                    ),
+                                  )
+                                ],
+                              );
+                            }
+                        );
+                      }
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-                      SizedBox(width: 10,height: 10,),
+//선호 스타일(여)
+class prefstylepagew extends StatefulWidget {
+  const prefstylepagew({Key? key}) : super(key: key);
 
+  @override
+  State<prefstylepagew> createState() => _prefstylepagewState();
+}
+
+class _prefstylepagewState extends State<prefstylepagew> {
+  UserDataFromServer userController = Get.find<UserDataFromServer>();
+  int selectedButtonIndex = -1; //선택된 버튼의 인덱스 (-1은 아무 버튼도 선택되지 않음을 의미)
+
+  void _handleButtonPress(int index) {
+    setState(() {
+      selectedButtonIndex = index;
+      if (index == 0){
+        UserInfo[2] = 'Casual';
+      }
+      else if (index == 1){
+        UserInfo[2] = 'Street';
+      }
+      else if(index == 2){
+        UserInfo[2] = 'Amekaji';
+      }
+      else if(index == 3){
+        UserInfo[2] = 'Classic';
+      }
+      else if(index == 4){
+        UserInfo[2] = 'GoffCore';
+      }
+      else if(index == 5){
+        UserInfo[2] = 'Lovely';
+      }
+      else{
+        UserInfo[2] = 'Sports';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image:DecorationImage(
+                image:AssetImage('assets/background_image.png'),
+                fit: BoxFit.cover
+            )
+        ),
+        child: Column(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 25,),
+                Text('선호 스타일', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: new LinearPercentIndicator(
+                    width: MediaQuery.of(context).size.width - 20,
+                    lineHeight: 10,
+                    percent: 3/3,
+                    backgroundColor: Colors.black12,
+                    progressColor: Colors.black,
+                    linearStrokeCap: LinearStrokeCap.roundAll,
+                  ),
+                )
+              ],
+            ),
+            Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  margin: EdgeInsets.all(20),
+                  child: Text('선호 스타일이 무엇인가요?', style: TextStyle(fontSize: 25, fontFamily: 'Dovemayo_gothic')),
+                )
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () => _handleButtonPress(0),
+                            style: ElevatedButton.styleFrom(
+                                primary: selectedButtonIndex == 0? Colors.grey : Color(0xffFFFAF3),
+                                foregroundColor: Colors.black,
+                                minimumSize: Size(250, 150),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(7)
+                                )
+                            ),
+                            child: Text('캐주얼', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
+                          ),
+                        ),
+
+                        SizedBox(width: 10,height: 10,),
+
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () => _handleButtonPress(1),
+                            child: Text('스트릿', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
+                            style: ElevatedButton.styleFrom(
+                                primary: selectedButtonIndex == 1? Colors.grey : Color(0xffFFFAF3),
+                                foregroundColor: Colors.black,
+                                minimumSize: Size(150, 150),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(7)
+                                )
+                            ),
+                          ),
+                        ),
+                      ]
+                  ),
+
+                  SizedBox(height: 10,),
+                  Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () => _handleButtonPress(2),
+                            child: Text('아메카지', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
+                            style: ElevatedButton.styleFrom(
+                                primary: selectedButtonIndex == 2? Colors.grey : Color(0xffFFFAF3),
+                                foregroundColor: Colors.black,
+                                minimumSize: Size(150, 150),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(7)
+                                )
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10,height: 10,),
+
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () => _handleButtonPress(3),
+                            child: Text('클래식', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
+                            style: ElevatedButton.styleFrom(
+                                primary: selectedButtonIndex == 3? Colors.grey : Color(0xffFFFAF3),
+                                foregroundColor: Colors.black,
+                                minimumSize: Size(150, 150),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(7)
+                                )
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(width: 10,height: 10,),
+
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () => _handleButtonPress(4),
+                            child: Text('고프코어', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
+                            style: ElevatedButton.styleFrom(
+                                primary: selectedButtonIndex == 4? Colors.grey : Color(0xffFFFAF3),
+                                foregroundColor: Colors.black,
+                                minimumSize: Size(150, 150),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(7)
+                                )
+                            ),
+                          ),
+                        ),
+                      ]
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
                       Expanded(
                         child: ElevatedButton(
+                          onPressed: () => _handleButtonPress(5),
+                          child: Text('러블리', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
                           style: ElevatedButton.styleFrom(
-                              primary: selectedButtonIndex == 7? Colors.grey : Color(0xffFFFAF3),
+                              primary: selectedButtonIndex == 5? Colors.grey : Color(0xffFFFAF3),
                               foregroundColor: Colors.black,
                               minimumSize: Size(150, 150),
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(7)
                               )
                           ),
-                          onPressed: () => _handleButtonPress(7),
-                          child: Text('캐주얼', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
                         ),
                       ),
-
                       SizedBox(width: 10,height: 10,),
 
                       Expanded(
                         child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                              primary: selectedButtonIndex == 8? Colors.grey : Color(0xffFFFAF3),
+                              primary: selectedButtonIndex == 6? Colors.grey : Color(0xffFFFAF3),
                               foregroundColor: Colors.black,
                               minimumSize: Size(150, 150),
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(7)
                               )
                           ),
-                          onPressed: () => _handleButtonPress(8),
-                          child: Text('캐주얼', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
+                          onPressed: () => _handleButtonPress(6),
+                          child: Text('스포츠', style: TextStyle(fontSize: 20, fontFamily: 'Dovemayo_gothic')),
                         ),
-                      )
+                      ),
                     ],
                   ),
                   SizedBox(height: 10,),
